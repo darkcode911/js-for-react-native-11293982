7db129ad5a2fc2826b6fad8add4c6fc2c399d29f
Henry Quaye <125556097+henryquayeesq@users.noreply.github.com>
2024-05-18 01:44:09 +0100

    Task 2

--- README.md
+++ README.md
@@ -3,7 +3,16 @@ Student ID: 11293982
 Dcit 202 Assignment 1 
 Array Manipulation and User Profile Creation 
 
-Task 1: Process Array of Numbers Description:
+Task 1: Process Array of Numbers 
+Description:
  The `processArray` function takes an array of numbers and returns a new array where:
  - Each even number is squared. 
  - Each odd number is tripled.
+
+Task 2: Format an array of strings with their corresponding array of numbers
+Description :
+The `formatArrayStrings` function takes two arrays:
+- An array of strings and An array of numbers processed by `processArray`. 
+ It modifies each string based on its corresponding number: 
+ - Capitalize the entire string if the number is even and Convert the string to lowercase if the number is odd.
+ 
--- arrayManipulation.js
+++ arrayManipulation.js
@@ -20,3 +20,33 @@ return newArray;
 const myArray = [11, 14, 35, 6, 7, 8];
 const result = processArray(myArray);
 console.log(result);
+
+//function formatArrayStrings which modifies strings based on its corresponding numbers
+
+function  formatArrayStrings(stringArr,numberArr) {
+
+    if (stringArr.length !== numberArr.length){
+    throw new Error ("input arrays must have equal length");
+    }
+    const formattedStrings=[];
+    for (let i=0;i< stringArr.length;i++) {
+        const currentString = stringArr[i];
+        const correspondingNumber = numberArr[i];
+        let formattedString;
+        if(correspondingNumber%2===0) {
+            formattedString = currentString.toUpperCase(); 
+        }
+       else {
+        formattedString = currentString.toLowerCase();
+       }
+       formattedStrings.push(formattedString); 
+    }
+    return formattedStrings;
+}
+//My trial inputs
+const myNumber = [9,14,35,6,7,8];
+const resultingNumbers = processArray(myArray);
+const lettersEdit = ["Sky", "Hack", "Life","Gold","Home","City"];
+const resultingFormat = formatArrayStrings(lettersEdit,resultingNumbers);
+
+console.log(resultingFormat);
